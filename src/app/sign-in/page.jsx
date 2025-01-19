d5d63f2e3178f51b05d31e534d50008a58b66e50
Prajualit Tickoo <23bec072@nith.ac.in>
2025-01-20 00:02:30 +0530

    google auth setup

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -1,11 +1,17 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import LoadingButton from "@/components/Loadingbutton";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import dotenv from "dotenv";
+import { gapi } from "gapi-script";
+
+dotenv.config();
 
 const page = () => {
 
@@ -48,46 +54,71 @@ const page = () => {
     finally {
       setPending(false);
     }
-  }
+  };
 
+  const clientId = "583655651500-ha5l0p0l2i2dg62hsomekir9q8jphr3q.apps.googleusercontent.com";
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Card className="w-full max-w-md rounded-xl shadow-md">
-        <CardHeader>
-          <CardTitle>Sign In</CardTitle>
-          <CardDescription>Log Into Your Account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} noValidate>
-            {["email", "password"].map((field) => (
-              <div key={field} className="mb-4">
-                <label htmlFor={field} className="block text-sm font-medium text-gray-700">
-                  {field.charAt(0).toUpperCase() + field.slice(1)}
-                </label>
-                <Input
+    <GoogleOAuthProvider clientId={`${clientId}`}>
+      <div className="h-screen flex justify-center items-center">
+        <Card className="w-full max-w-md rounded-xl shadow-md">
+          <CardHeader>
+            <CardTitle>Sign In</CardTitle>
+            <CardDescription>Log Into Your Account</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+              {["email", "password"].map((field) => (
+                <div key={field} className="mb-4">
+                  <label htmlFor={field} className="block text-sm font-medium text-gray-700">
+                    {field.charAt(0).toUpperCase() + field.slice(1)}
+                  </label>
+                  <Input
 
-                  type={field === "password" ? "password" : "text"}
-                  label={field}
-                  {...register(field, {
-                    required: field === "email" ? "Email is required" : "Password is required",
-                  })}
-                  placeholder={`Enter your ${field}`}
-                  error={errors[field]}
-                />
+                    type={field === "password" ? "password" : "text"}
+                    label={field}
+                    {...register(field, {
+                      required: field === "email" ? "Email is required" : "Password is required",
+                    })}
+                    placeholder={`Enter your ${field}`}
+                    error={errors[field]}
+                  />
+                </div>
+              ))}
+              <LoadingButton
+                type="submit"
+                pending={pending}
+              >
+                Sign In
+              </LoadingButton>
+              <div className="text-center text-[16px] text-gray-700 font-medium w-full my-2 flex items-center justify-between">
+                <span className="w-[45%] border-[1px] "></span>
+                <span>
+                  or
+                </span>
+                <span className="w-[45%] border-[1px] "></span>
               </div>
-            ))}
-            <LoadingButton
-              type="submit"
-              pending={pending}
-              className="w-full mt-4"
-            >
-              Sign In
-            </LoadingButton>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+              {/* <LoadingButton
+                type="submit"
+                pending={pending}
+                href="http://localhost:5000/auth/google">
+                Sign In with Google
+              </LoadingButton> */}
+              <GoogleLogin
+                clientId={clientId}
+                onSuccess={(res) => console.log(res)}
+                onError={() => "Login Failed"}
+              />
+            </form>
+            <div className="mt-4 text-center text-sm">
+              <Link href="/sign-up" className="text-primary hover:underline">
+                Don't have an account? Sign up
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </GoogleOAuthProvider>
   )
 }
 
