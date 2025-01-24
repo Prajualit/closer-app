27dee035b7b5a72b8d36da3aeaa9941c568b6189
Prajualit Tickoo <23bec072@nith.ac.in>
2025-01-24 23:38:28 +0530

    database

--- src/backend/db.js
+++ src/backend/db.js
@@ -4,7 +4,18 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const mongoDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI, ()=> {
+    if (err) {
+      console.log('Error', err);
+  } else {
+      console.log('Connected');
+      const fetched_user = mongoose.connection.db.collection("users");
+      fetched_user.findOne({}).toArray(async function (err, data) {
+        const user = await data;
+          return user;
+      })
+    }
+  });
 };
 
 module.exports = mongoDB;
