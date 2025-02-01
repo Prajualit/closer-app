6d1f83100123bae166bed9e1ad13a593ce15c3ff
Prajualit Tickoo <23bec072@nith.ac.in>
2025-02-01 13:58:01 +0530

    fix 2.1

--- src/backend/controllers/user.controllers.js
+++ src/backend/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
+import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // Requesting User Data from frontend
@@ -12,19 +13,34 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   //Duplicate responses check
-  const existedUser = User.findOne({ 
+  const existedUser = User.findOne({
     $or: [{ username }, { email }],
-   });
+  });
   if (existedUser) {
     throw new apiError(409, "User already exists");
   }
 
+  //   create user in database
   const user = await User.create({
     username,
     email,
     password,
-  })
+  });
 
+  //   remove password and refreshtoken from response
+  const createdUser = await User.findbyId(user._id).select(
+    "-password -refreshToken"
+  );
+
+  //   Error handling
+  if (!createdUser) {
+    throw new apiError(500, "Something went wrong while registering the user");
+  }
+
+  //   return a response
+  return res
+    .status(201)
+    .json(new apiResponse(200, createdUser, "User Registered Successfully"));
 });
 
 export { registerUser };
