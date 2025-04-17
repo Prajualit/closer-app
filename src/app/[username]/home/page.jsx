d1672064b4a6ce2539c513cc89710cf97d5c356a
Prajualit Tickoo <23bec072@nith.ac.in>
2025-04-17 23:26:45 +0530

    home

--- src/app/[username]/home/page.jsx
+++ src/app/[username]/home/page.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import Navbar from '@/components/HomePg/Navbar'
+import Navbar from '@/components/HomePg/Navbar.jsx'
+import Navsearch from '@/components/HomePg/Navsearch.jsx'
+import { useSelector } from 'react-redux'
+import Photo from '@/components/ui/photo.jsx'
 
 const page = () => {
     return (
-        <div className=''>
+        <div className='flex'>
             <Navbar />
+            <div className='ml-[15rem] flex items-center justify-center w-full p-10 space-x-10 overflow-y-auto '>
+                <div className='w-full h-full flex flex-col items-center justify-start rounded-xl '>
+                    <div className='w-full flex items-center justify-center pb-5 '>
+                        <Navsearch />
+                    </div>
+                    <Photo />
+                    <Photo />
+                    <Photo />
+                </div>
+                <div className='w-[50%] flex items-center justify-center bg-[#efefef] rounded-xl'>
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/HomePg/Navsearch.jsx
+++ src/components/HomePg/Navsearch.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const Navsearch = () => {
 
     return (
-        <div className='border transition-all duration-300 hover:shadow-md focus-within:shadow-md flex items-center rounded-full px-3 py-2 bg-[#e8e8e8] space-x-2 w-[30%] '>
+        <div className='border border-[#cacaca] w-[75%] transition-all duration-300 hover:shadow-md focus-within:shadow-md flex items-center rounded-full px-3 py-2 bg-[#f7f7f7] space-x-2 '>
             <Image src={search} alt="" />
             <input
                 placeholder='Search'
