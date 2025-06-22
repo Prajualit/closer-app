d91120c5e58c3fe494a34d5fde3db2fde8c28a72
Prajualit Tickoo <23bec072@nith.ac.in>
2025-06-23 00:12:41 +0530

    routes

--- src/backend/Routes/user.routes.js
+++ src/backend/Routes/user.routes.js
@@ -4,22 +4,24 @@ import {
   getCurrentUser,
   loginUser,
   logoutUser,
-  refreshAccessToken
+  refreshAccessToken,
+  editUser,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router
-  .route("/register")
-  .post(upload.single("avatarUrl"), registerUser);
+router.route("/register").post(upload.single("avatarUrl"), registerUser);
 router.route("/login").post(loginUser);
 router.route("/refresh-token").post(refreshAccessToken);
 
-// secured routes
 
+// secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/getUser").get(verifyJWT, getCurrentUser);
+router
+  .route("/update-user")
+  .post(verifyJWT, upload.single("avatarUrl"), editUser);
 
 export default router;
